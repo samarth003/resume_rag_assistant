87dc6b8970fb9a1f720e99b8a5659d86f709d4f3
Samarth Kapoor <samarthkapoor@live.com>
2025-05-09 10:44:45 -0400

    1.Added hardcoded chunks to check if the model is working on those. 2. Added require keyword for comparative queries 3. Textwrapped the prompt for better indented query model

--- promptbuilder.py
+++ promptbuilder.py
@@ -1,19 +1,28 @@
-import re
+import re, textwrap
 
 def is_comparative_query(user_query):
     query = user_query.lower()
     keywords = [
         "missing", "compare", "not in my resume", "lacking",
-        "gaps", "not mentioned", "difference", "absent"
+        "gaps", "not mentioned", "difference", "absent", "require"
     ]
     return any(term in query for term in keywords)
 
 def build_prompt(user_query, resume_chunks, jd_chunks=None):
+    resume_chunks = [
+        "Developed embedded software for ARM Cortex-M microcontrollers.",
+        "Worked with C and bare-metal environments for performance-critical applications."
+        ]
+    jd_chunks = [
+        "Looking for someone with experience in RTOS, embedded Linux, and ARM-based firmware development."
+        ]
+    if not resume_chunks:
+        return f"Resume content is empty. Cannot generate an answer."
     r_text = "\n".join(resume_chunks)
 
     if is_comparative_query(user_query=user_query) and jd_chunks:
         jd_text = "\n".join(jd_chunks)
-        return f"""
+        return textwrap.dedent(f"""
         Compare the user's resume and job description below:
 
         Resume:
@@ -27,9 +36,9 @@ def build_prompt(user_query, resume_chunks, jd_chunks=None):
 
         Instruction:
         List specific skills present in job description but missing in resume.
-        """
+        """)
     else:
-        return f"""
+        return textwrap.dedent(f"""
         You are an assistant evaluating user's resume.
 
         Here is the resume context:
@@ -39,5 +48,5 @@ def build_prompt(user_query, resume_chunks, jd_chunks=None):
         {user_query}
 
         Instruction:
-        Answer using the resume above. If something is not explicitly mentioned, say so clearly
-        """
+        Only answer using the resume above. If something is not explicitly mentioned, say so clearly and do not assume.
+        """)
